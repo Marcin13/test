2816a4b89cb0edc5e4bb4d83bcd95ebe14473757
marcin <marcin@marcin-Komputer.home>
2018-01-08 20:35:07 +0000

    dodajemy style

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
  * and open the template in the editor.
  * 
  * 
- * wczesniej trzeba zrobic commit changes
+ * wczesniej trzeba zrobic commit changes,
+ * do nowych plików tez trzeba wykonac tylko commit..i wszystko dziala. 
  * Zrobilem  push razem ze swoim login i haslo
  */
 
